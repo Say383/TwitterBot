7abc805514e0454c61e66ad5872748bbc271b818
Say <said.mansabdar@gmail.com>
2024-01-26 11:14:11 +0100

    Update user.py

--- src/models/user.py
+++ src/models/user.py
@@ -1,5 +1,7 @@
-
-class User:
-    def __init__(self, user_id, username):
-        self.user_id = user_id
-        self.username = username
+# src/models/user.py
+class User(Base):
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
+    email = Column(String, unique=True)
+    is_active = Column(Boolean, default=True)
+    last_login = Column(DateTime)  # New field added
