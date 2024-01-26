99dfb54e255c58a2cdb5ea44dbe265a1924b4662
Say <said.mansabdar@gmail.com>
2024-01-26 11:11:40 +0100

    Update recovery.py

--- src/disaster_recovery/recovery.py
+++ src/disaster_recovery/recovery.py
@@ -1,7 +1,8 @@
+import subprocess
 
 # Function to perform recovery from a backup file
-def perform_recovery(backup_file, database_uri):
-    command = f'mysql -u user -p'password' {database_uri} < {backup_file}'
+def perform_recovery(backup_file, database_uri, username, password):
+    command = f'mysql -u {username} -p{password} {database_uri} < {backup_file}'
     subprocess.run(command, shell=True)
     print(f'Recovery performed from {backup_file}')
 
@@ -11,4 +12,8 @@ def test_recovery():
     pass
 
 # Call the function to perform recovery (example usage)
-perform_recovery('/mnt/data/backups/db_backup_latest.sql', 'my_database_uri')
+username = 'user'
+password = 'password'
+database_uri = 'my_database_uri'
+backup_file = '/mnt/data/backups/db_backup_latest.sql'
+perform_recovery(backup_file, database_uri, username, password)
